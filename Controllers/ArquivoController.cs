c7f0455d1cdca9d55edf4045996c05055298d42f
Maykon Ferreira <74366853+MaykonFerrero@users.noreply.github.com>
2021-05-08 21:39:12 -0400

    Update4 ArquivoController.cs
    Adicionado outros comentários para facilitar interpretação

--- Controllers/ArquivoController.cs
+++ Controllers/ArquivoController.cs
@@ -10,14 +10,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Gerenciador_Arquivos.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] // define que a rota ou os próximos passos são controllers de uma api
+    [ApiController] // Informa que todos as ações são de Controlador
     public class ArquivoController : ControllerBase
     {
         private static string diretorio;
         private SHA256 Sha256 = SHA256.Create(); //cria uma instância para implementar o SHA256
 
-        public ArquivoController()
+        public ArquivoController() // cria um método público responsável por criar o diretório onde ficarão armazenados os arquivos
         {
             diretorio = Path.Combine(Directory.GetCurrentDirectory(), "arquivos"); //guarda em diretorio o caminho para a pasta chamada de "arquivos" 
 
@@ -27,28 +27,29 @@ namespace Gerenciador_Arquivos.Controllers
             }
         }
 
-        [HttpGet]
+        [HttpGet] // informa que as próximas ações são de HTTP GET
         public string Get()
         {
             string resposta = "Gerenciador de Arquivos em execução: " + DateTime.Now.ToLongTimeString();
             resposta += $"\n Diretorio de arquivos: \"{diretorio}\"";
-            return resposta;
+            return resposta; // informa quando foi realizada a execução e em que local do diretório estão os arquivos que foram upados
         }
 
-        [HttpPost("upload")]
+        [HttpPost("upload")] // Informa que a próxima ação é um HTTP POST
         public async Task<string> EnviaArquivo([FromForm] IFormFile arquivo) // espera de maneira assíncrona até que um arquivo seja enviado
         {
-            if (arquivo.Length > 0) //quando recebe um arquivo de tamanho > 0 
+            if (arquivo.Length > 0) 
             {
                 try // é necessário ter um método try catch pois podem ocorrer excessões
                 {
                     using (FileStream filestream = System.IO.File.Create(Path.Combine(diretorio, arquivo.FileName)))
+                    // faz um stream do arquivo possibilitando as operações de leitura
                     {
-                        await arquivo.CopyToAsync(filestream);
-                        filestream.Flush();
+                        await arquivo.CopyToAsync(filestream); //Lê de forma assíncrona os bytes do fluxo atual(filestream) 
+                        filestream.Flush(); //Limpa os buffers do fluxo atual e faz com que todos os dados armazenados em buffer sejam gravados no arquivo (filestream)
 
-                        byte[] hash = Sha256.ComputeHash(arquivo.OpenReadStream());
-                        string strHash = BytesToString(hash);
+                        byte[] hash = Sha256.ComputeHash(arquivo.OpenReadStream()); //Calcula o Hash do arquivo e retorna um array de bytes 
+                        string strHash = BytesToString(hash); //transforma esse array de byte em string
 
                         string resposta = "Arquivo recebido.";     // a variável resposta contém a sinalização de que o arquivo foi recebido
                         resposta += $"\nNome: {arquivo.FileName}";
@@ -68,7 +69,7 @@ namespace Gerenciador_Arquivos.Controllers
             }
         }
 
-        public static string BytesToString(byte[] bytes)
+        public static string BytesToString(byte[] bytes) // método que retorna um array  com uma sequência de valores hexadecimais
         {
             string result = "";
             foreach (byte b in bytes)
